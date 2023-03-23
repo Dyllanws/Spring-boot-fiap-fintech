8742fcdcf9eb6899ca19c5da4d0239d09e61a3f3
Dyllan Christian <dyllan.cscc@outlook.com>
2023-03-23 14:14:40 -0300

    iniciando API Spring

---
+++ src/main/java/com/fiap/dyllanws/fiapFintech/FiapFintechApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.dyllanws.fiapFintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapFintechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapFintechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/dyllanws/fiapFintech/FiapFintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.dyllanws.fiapFintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapFintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
